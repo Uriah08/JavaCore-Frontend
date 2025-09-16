07f7f80f76c1bebc01bbc341b63dc42186b805b2
Uriah08 <lorenz8.flores@gmail.com>
2025-09-16 18:44:16 +0800

    Authentication

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
 
-export const formSchema = z.object({
-  name: z.string().min(2).max(50),
-  email: z.string().min(2)
+export const loginSchema = z.object({
+  email: z.string().min(2),
+  password: z.string().min(5)
 })
